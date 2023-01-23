be84f20c3753a491f25e61b995edad3e795d701a
Alex G <a.grigoriev@ze.studio>
2023-01-23 09:24:23 +0300

    test peparation for vyyer

--- tests/test.py
+++ tests/test.py
@@ -2,19 +2,19 @@ import pytest
 from selenium.webdriver.common.by import By
 
 def test_example(selenium):
-    selenium.get('https://bstackdemo.com/')
+    selenium.get("https://websdk.dev.vyyer.id/")
 
     # locating product on webpage and getting name of the product
-    productText = selenium.find_element(By.XPATH, '//*[@id="1"]/p').text
+    #productText = selenium.find_element(By.XPATH, '//*[@id="1"]/p').text
 
-    # clicking the 'Add to cart' button
-    selenium.find_element(By.XPATH, '//*[@id="1"]/div[4]').click()
+    # clicking the "full KYC button on the page"
+    selenium.find_element(By.XPATH, "//div[@id='app']/div/div/div[2]/div[2]/div/div[2]").click()
 
-    # waiting until the Cart pane has been displayed on the webpage
-    selenium.find_element(By.CLASS_NAME, 'float-cart__content')
+    # then we'll click on the element on the widget, the same KYC
+    selenium.find_element(By.XPATH, "//div[@id='app']/div/div/vyyer-widget").click()
 
     # locating product in cart and getting name of the product in cart
-    productCartText = selenium.find_element(By.XPATH, '//*[@id="__next"]/div/div/div[2]/div[2]/div[2]/div/div[3]/p[1]').text
+    #productCartText = selenium.find_element(By.XPATH, '//*[@id="__next"]/div/div/div[2]/div[2]/div[2]/div/div[3]/p[1]').text
 
     # checking whether product has been added to cart by comparing product name
-    assert productCartText == productText
+    #assert productCartText == productText
